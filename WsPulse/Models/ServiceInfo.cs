4cf6a5220df9f14a179d43d84e7f60a5d76ceb3e
Edd <103119736+MrSatisfactionGuy@users.noreply.github.com>
2025-11-06 12:26:02 +0100

    initial commit

---
+++ WsPulse/Models/ServiceInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace WsPulse.Models;
+
+public class ServiceInfo
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
+    public bool IsReachable { get; set;}
+    public DateTime LastChecked { get; set;}
+}
