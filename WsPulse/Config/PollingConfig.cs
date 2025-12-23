88969953a061c02db578557e8602ab14aa86c4b1
Edd <103119736+MrSatisfactionGuy@users.noreply.github.com>
2025-12-23 15:54:18 +0100

    Add files via upload

---
+++ WsPulse/Config/PollingConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace WsPulse.Config;
+
+public class PollingConfig
+{
+    public int IntervalSeconds { get; set; } = 60;
+    public int TimeoutSeconds { get; set; } = 5;
+    public int MaxParallel { get; set; } = 10;
+}
